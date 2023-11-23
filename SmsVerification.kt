d87a592a6c55f8dd9fe8b9ab341d658ec62fe09d
Jahongir Tolibov <jahongir17tolibov@gmail.com>
2023-11-23 16:22:58 +0500

    Bir balo lar....

--- SmsVerification.kt
+++ SmsVerification.kt
@@ -1,3 +1,10 @@
 fun main() {
     val list = listOf()
+    val cdwdc = listOf()
+    val cwcww = listOf()
+    val  cefceve = listOf()
+    val lisce = listOf()
+    val dccewrbetrny = listOf()
+    val njkcn0 = listOf()
+    val nkwnfkjn = listOf()
 }
