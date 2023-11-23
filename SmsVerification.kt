51d23d8a1092148439797a92519819e0592ded12
Jahongir Tolibov <jahongir17tolibov@gmail.com>
2023-11-23 16:40:12 +0500

    for merging

--- SmsVerification.kt
+++ SmsVerification.kt
@@ -1,22 +1,15 @@
 fun main() {
     val list = listOf()
-<<<<<<< HEAD
     val cdwdc = listOf()
     val cwcww = listOf()
-    val  cefceve = listOf()
-    val lisce = listOf()
-    val dccewrbetrny = listOf()
-    val njkcn0 = listOf()
 
     for (i in list.indicies) {
         print(i)
-=======
 
     if (list.IsEmpty) print(true)
 
     list.forEach {
         it++
->>>>>>> master
     }
 
 }
